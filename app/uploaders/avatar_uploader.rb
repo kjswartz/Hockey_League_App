e3b8390f569579c86825bdcd9e81769b476ed5b3
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-30 14:49:25 -0400

    added default avatar silhouette.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -25,27 +25,21 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+  process :resize_to_fit => [200, 250]
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [75, 75]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  # default image
+  def default_url
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "male-silhouette.png"].compact.join('_'))
+  end
 
 end
