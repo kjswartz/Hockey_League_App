a3f880d1d4cd0f4bde28692137acfef5e94fba73
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-30 17:50:41 -0400

    setup current_user in application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :current_user
+
+  def authenticate_admin_user!
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  helper_method :signed_in?, :current_user
+
+  def signed_in?
+    return true if current_user.present?
+  end
+
 end
