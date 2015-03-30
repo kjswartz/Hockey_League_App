347fe21586f752643afe017626679c7d2900aa61
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-30 17:53:01 -0400

    setup admin user view.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,30 @@
 ActiveAdmin.register User do
 
+  permit_params(:name, :email, :avatar, :avatar_cache, :bio, :remove_avatar)
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  scope :admin
+  scope :non_admin
 
+  form do |f|
+    inputs '' do
+      input :name
+      input :email
+      input :avatar
+      input :bio
+    end
+    actions
+  end
+
+  index do
+    selectable_column
+    column :id
+    column "Profile Pic" do |user|
+      image_tag user.avatar.url(:thumb)
+    end
+    column :name
+    column :email
+    column :bio
+    actions
+  end
 
 end
