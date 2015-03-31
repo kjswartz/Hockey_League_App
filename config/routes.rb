4e3bcb56dbb4a69c6e0812861f216b7df0cb9b0e
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-31 19:01:19 -0400

    removed unneeded routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   post 'session/signin'
   delete 'session/signout'
-  
+
   ActiveAdmin.routes(self)
   root 'welcome#index'
-  resources :leagues
-  resources :teams
+
+  resources :leagues, only: [:index, :show]
+  resources :teams, only: [:show]
   resources :users
 
 end
