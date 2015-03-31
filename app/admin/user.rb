f68c659a36ec6d9344d4b0ea2c17d43f73170cbb
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-31 11:59:34 -0400

    added team and league view to admin. linked to pages.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,12 +1,13 @@
 ActiveAdmin.register User do
 
-  permit_params(:name, :email, :avatar, :avatar_cache, :bio, :remove_avatar, :active)
+  permit_params(:name, :email, :avatar, :avatar_cache, :bio, :remove_avatar, :active, :team_ids, team_ids: [])
 
   form do |f|
     inputs '' do
       input :name
       input :email
       input :avatar
+      input :teams, :as => :check_boxes, :collection => Team.order("name ASC").all
       input :bio
     end
     actions
@@ -20,7 +21,13 @@ ActiveAdmin.register User do
     end
     column :name
     column :email
-    column :bio
+    column 'Leagues' do |user|
+      user.teams.map { |t| link_to t.league.skill, admin_league_path(t.league.id) }.join("<br>").html_safe
+    end
+    column 'Teams' do |user|
+      user.teams.map { |t| link_to t.name, admin_team_path(t.id) }.join("<br>").html_safe
+    end
+    column :active
     actions
   end
 
