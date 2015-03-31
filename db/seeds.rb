51c0e737a7d57929022467e2fa3ccefef32dbe63
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-31 19:02:11 -0400

    seeded 10 users to each team.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+@teams = Team.all
+
+@teams.each do |team|
+  puts "#{team.name} being seeded"
+  x = 0
+  10.times do |counter|
+    puts "User count #{x}"
+    User.create(name: Faker::Name.name,
+               email: Faker::Internet.safe_email,
+               remote_avatar_url: Faker::Avatar.image,
+               bio: Faker::Lorem.paragraph,
+               team_ids: team.id,
+               phone: Faker::PhoneNumber.cell_phone,
+               goals: rand(1..15),
+               assists: rand(1..10),
+               penalties: rand(1..20),
+               password: 'password')
+    x += 1
+  end
+end
