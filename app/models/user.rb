e03457cb49f612951555a32334de688cadf8998c
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-04-01 14:59:56 -0400

    validator to password on create only.

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
   #validations
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :create
 
   mount_uploader :avatar, AvatarUploader
 
