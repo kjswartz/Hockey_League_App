c34803402bb937eca1c0e8c5e954c5ad7ffcc2d7
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-04-02 00:30:29 -0400

    setup mailer for users requesting team access. setup form for users to submit with.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,12 @@ class UserMailer < ApplicationMailer
     @url = 'https://rocky-sea-8876.herokuapp.com/'
     mail(to: @user.email, subject: "Welcome to the Hockey League App")
   end
+
+  def request_team_access_email(user, team)
+    @user = user
+    @team = team
+    @url = 'https://rocky-sea-8876.herokuapp.com/'
+    mail(to: @team.owner, subject: "Welcome to the Hockey League App")
+  end
+
 end
