0accbb4634da7b3ad09bea2079fbd5f1e8e869d0
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-04-03 12:19:25 -0400

    added form to add event (game) to a team.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,44 @@
 class EventsController < ApplicationController
 
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
   def create
     @event = Event.new(event_params)
-    if @event.save
-      flash[:notice] = "Game Added!"
-      redirect_to root_url
-    else
-      flash[:notice] = "Unable to add game."
-      redirect_to root_url
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to '/', notice: 'Event was successfully created.' }
+      else
+        flash[:error] = "We were unable to add this event. #{@event.errors.full_messages.join('. ')}"
+        format.html { render :new }
+      end
     end
   end
-  
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @team, notice: 'Event was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:team_id, :opponent_id, :game_time)
+    end
+
 end
