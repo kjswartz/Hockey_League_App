47810cdbb9de1c57df9be1c9237d29e4573d1895
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-04-02 23:22:17 -0400

    annotated events model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  game_time   :datetime
+#  opponent_id :integer
+#  team_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Event < ActiveRecord::Base
   belongs_to :opponent, class: Team
   belongs_to :team
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  game_time   :datetime
+#  opponent_id :integer
+#  team_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
